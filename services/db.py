6ad32b1d9293a972bf16740d9129c5af004ebd5f
ねんねこ <suimaozzzz@gmail.com>
2025-08-04 02:07:29 +0900

    db.py を更新

--- services/db.py
+++ services/db.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import asyncpg
 
 from .env import Env
@@ -8,3 +10,8 @@ class DBService:
     @classmethod
     async def init(cls):
         cls.pool = await asyncpg.create_pool(Env.get("dsn"))
+        
+    @classmethod
+    async def close(cls):
+        async with asyncio.timeout(10):
+            await cls.pool.close()
