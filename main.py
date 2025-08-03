6548799d92f95b240aed1ce5bd516e7e81015426
ねんねこ <suimaozzzz@gmail.com>
2025-08-04 02:06:34 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from services.db import DBService
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await DBService.init()
+    yield
+    await DBService.close()
+
+app = FastAPI(lifespan=lifespan)
